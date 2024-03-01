7319cd7c1e79f0fd08e6d75de66348d0c97a2674
hengage <tunecarterkillz@gmail.com>
2024-03-01 02:00:48 +0100

    Init project

---
+++ hearts_and_hands/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for hearts_and_hands project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hearts_and_hands.settings')
+
+application = get_asgi_application()
