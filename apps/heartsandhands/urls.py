0188287bf8cd4fc4839bb742fe55b145151d6398
hengage <tunecarterkillz@gmail.com>
2024-03-03 23:30:32 +0100

    Feature: home template

--- apps/heartsandhands/urls.py
+++ apps/heartsandhands/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path, include
+
 from . import views
 
+
 urlpatterns = [
-    path('', views.HomeView.as_view(), name='home'),
+   path('', views.HomeView.as_view(), name='home'),
 ]
