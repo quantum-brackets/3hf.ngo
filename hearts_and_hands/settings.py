1eee71811c06286f06f0558b3a2c4078ae5c45f0
hengage <tunecarterkillz@gmail.com>
2024-03-04 12:03:40 +0100

    Feature: setup tailwind

--- hearts_and_hands/settings.py
+++ hearts_and_hands/settings.py
@@ -126,6 +126,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = str(BASE_DIR.joinpath('staticfiles'))
+STATICFILES_DIRS = [str(BASE_DIR.joinpath('static'))]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
