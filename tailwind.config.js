8213ee6b25ae8c45157927515e732083105fbfe1
hengage <tunecarterkillz@gmail.com>
2024-03-10 00:53:32 +0100

    Styles: extend text color

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,10 @@ module.exports = {
     extend: {
       fontFamily: {
         inter: ['Inter', 'sans-serif'],
-      }
+      },
+      colors: {
+        'text-color': '#6D6D6D',
+      },
     },
   },
   plugins: [],
