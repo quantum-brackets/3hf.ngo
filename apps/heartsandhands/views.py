a574592f2c877f73efdadb98d9fabe4d7658bf1b
hengage <tunecarterkillz@gmail.com>
2024-03-16 17:06:28 +0100

    Feature: dynamic bg color

--- apps/heartsandhands/views.py
+++ apps/heartsandhands/views.py
@@ -4,5 +4,17 @@ from django.views.generic import TemplateView
 class HomeView(TemplateView):
     template_name = 'heartsandhands/home.html'
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        # Set the background color for the home view
+        context['bg_color'] = '#FCF7CC'
+        return context
+
+
 class ContactUsView(TemplateView):
     template_name = "heartsandhands/contact_us.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['bg_color'] = '#06589C'
+        return context
