7a027f321fd350bbb5bd13450e4fdb2e4f76d406
hengage <tunecarterkillz@gmail.com>
2024-04-15 14:10:42 +0100

    Feature: implement active button styling

--- static/js/donation.js
+++ static/js/donation.js
@@ -1,13 +1,31 @@
-const paystackForm = document.getElementById('paystack-form');
-const stripeForm = document.getElementById('stripe-form');
+const paystackForm = document.getElementById("paystack-form");
+const stripeForm = document.getElementById("stripe-form");
 
+const showPaystackFormButton = document.getElementById("show-paystack-form");
+
+const showStripeFormButton = document.getElementById("show-stripe-form");
+
+console.log(showPaystackFormButton);
+console.log(showPaystackFormButton);
 
 function showPaystackForm() {
-    paystackForm.classList.remove("hidden");
-    stripeForm.classList.add("hidden");
-  }
+  paystackForm.classList.remove("hidden");
+  stripeForm.classList.add("hidden");
 
-  function showStripeForm() {
-    paystackForm.classList.add("hidden");
-    stripeForm.classList.remove("hidden");
-  }
+  showPaystackFormButton.classList.add("border-[#2b599c]");
+  showPaystackFormButton.classList.remove("border-[#DFDFDF]");
+
+  showStripeFormButton.classList.add("border-[#DFDFDF]");
+  showStripeFormButton.classList.remove("border-[#2b599c]");
+}
+
+function showStripeForm() {
+  paystackForm.classList.add("hidden");
+  stripeForm.classList.remove("hidden");
+
+  showPaystackFormButton.classList.remove("border-[#2b599c]");
+  showPaystackFormButton.classList.add("border-[#DFDFDF]");
+
+  showStripeFormButton.classList.add("border-[#2b599c]");
+  showStripeFormButton.classList.remove("border-[#DFDFDF]");
+}
