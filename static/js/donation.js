d3b7e7e85167be3032c145ffbc4ea9cef91b579e
hengage <tunecarterkillz@gmail.com>
2024-04-19 14:07:00 +0100

    Chore: work on donation page and fix stripe payment validation

--- static/js/donation.js
+++ static/js/donation.js
@@ -1,10 +1,11 @@
+"use strict";
+
 const paystackForm = document.getElementById("paystack-form");
 const stripeForm = document.getElementById("stripe-form");
 
 const showPaystackFormButton = document.getElementById("show-paystack-form");
 
 const showStripeFormButton = document.getElementById("show-stripe-form");
-
 
 function showPaystackForm() {
   console.log("testing");
@@ -29,13 +30,12 @@ function showStripeForm() {
   showStripeFormButton.classList.remove("border-[#DFDFDF]");
 }
 
-
 var paymentForm = document.getElementById("paystack-form");
 paymentForm.addEventListener("submit", payWithPaystack, false);
 
 function payWithPaystack() {
-  const validate =  validatePaystack();
-  if (validate === false){
+  const validate = validatePaystackForm();
+  if (validate === false) {
     return;
   }
 
@@ -45,7 +45,7 @@ function payWithPaystack() {
     amount: document.getElementById("amount").value * 100, // the amount value is multiplied by 100 to convert to the lowest currency unit
     currency: document.getElementById("currency").value,
     // csrfmiddlewaretoken: document.getElementsByName('csrfmiddlewaretoken')[0].value
-    ref: generateReference, 
+    ref: generateReference,
     callback: handlePaymentCallback,
     onClose: function () {
       alert("Transaction was not completed, window closed.");
@@ -72,12 +72,12 @@ function verifyPayment(reference) {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
-      "X-CSRFToken": document.getElementsByName('csrfmiddlewaretoken')[0].value,
+      "X-CSRFToken": document.getElementsByName("csrfmiddlewaretoken")[0].value,
     },
     body: JSON.stringify({ reference }),
   }).then((response) => {
     if (response.ok) {
-      console.log({"Server response": response});
+      console.log({ "Server response": response });
       alert("Donation successful! Thank you.");
     } else {
       console.error("Error submitting donation data:", response.statusText);
@@ -86,48 +86,74 @@ function verifyPayment(reference) {
   });
 }
 
-async function generateReference () {
-  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-  let reference = '';
+async function generateReference() {
+  const characters =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  let reference = "";
 
   // Generate reference with a combination of numbers and letters
   for (let i = 0; i < length; i++) {
-      const randomIndex = Math.floor(Math.random() * characters.length);
-      reference += characters.charAt(randomIndex);
+    const randomIndex = Math.floor(Math.random() * characters.length);
+    reference += characters.charAt(randomIndex);
   }
 
   // Add current timestamp to the reference
   const timestamp = new Date().getTime().toString();
   reference += timestamp;
-  console.log({reference})
+  console.log({ reference });
 
   return reference;
 }
 
-function validatePaystack() {
-  console.log('Performing validation')
-  const amount = document.getElementById('amount').value;
-  const email = document.getElementById('email').value;
-  const error = document.getElementById("paystack-form-error");
-
-
+function validatePaystackForm(e) {
+  console.log("Performing validation");
+  const amount = document.getElementById("amount").value;
+  const email = document.getElementById("email").value;
+  const errorText = document.getElementById("paystack-form-error");
+ console.log({amount});
   if (!amount || !email) {
-      error.innerHTML = 'Please fill in all required fields.';
-      return false;
+    errorText.innerHTML = "Please fill in all required fields.";
+    return false;
   }
 
   const regex = /^[0-9]+$/;
 
   if (!regex.test(amount)) {
-      error.innerHTML = 'Please enter only numbers in the amount field.';
-      // Clear the input field or prevent further input
-      amount.value = amount.replace(/[^0-9]/g, '');
-      return false
+    errorText.innerHTML = "Please enter only numbers in the amount field.";
+    return false;
   }
 
-  if(amount <= 0) {
-    error.textContent = "Amount must be greater than 0"
-    return false
+  if (amount <= 0) {
+    error.textContent = "Amount must be greater than 0";
+    return false;
   }
-  return true
+  return true;
+}
+
+function validateStripeForm(e) {
+  console.log("Performing validation");
+  const amount = document.getElementById("stripe-amount").value;
+  console.log({amount});
+  const errorText = document.getElementById("stripe-form-error");
+
+  if (!amount) {
+    errorText.innerHTML = "Please fill in amount field.";
+    e.preventDefault();
+    return false;
+  }
+
+  const regex = /^[0-9]+$/;
+
+  if (!regex.test(amount)) {
+    errorText.innerHTML = "Please enter only numbers in the amount field.";
+    // Clear the input field or prevent further input
+    e.preventDefault();
+    return false;
+  }
+
+  if (amount <= 0) {
+    error.textContent = "Amount must be greater than 0";
+    return false;
+  }
+  return true;
 }
