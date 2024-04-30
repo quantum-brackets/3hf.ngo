771d3aac1e8c7f2262acfe487411794d29a89b2d
hengage <tunecarterkillz@gmail.com>
2024-04-30 14:53:13 +0100

    Chore: media upload directory

--- apps/events/models.py
+++ apps/events/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 from django.template.defaultfilters import slugify
-import random
 
 class UpcomingEvents(models.Model):
     theme = models.CharField(max_length=200)
@@ -8,7 +7,7 @@ class UpcomingEvents(models.Model):
     time = models.TimeField()
     date = models.DateField()
     location = models.CharField(max_length=200)
-    image = models.ImageField(upload_to='charity_events_images')
+    image = models.ImageField(upload_to='static/uploads')
     createdAt = models.DateTimeField(auto_now_add=True)
     updatedAt = models.DateTimeField(auto_now=True)
     slug = models.SlugField(max_length=70, null=True, blank=True, editable=False)
@@ -18,7 +17,6 @@ class UpcomingEvents(models.Model):
         verbose_name_plural = "Upcoming events"
         
     def save(self, *args, **kwargs): 
-        random_string = ''.join(str(random.randint(0, 9)) for _ in range(5))
         slug = f'{self.theme}-{self.date}'
         if not self.slug:
             self.slug = slugify(slug)
