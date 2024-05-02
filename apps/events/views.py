ac6744fa8ea8bf37b2f72979c0b1db51e36040c1
hengage <tunecarterkillz@gmail.com>
2024-05-02 15:04:18 +0100

    Feature: template for events

--- apps/events/views.py
+++ apps/events/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
 
-# Create your views here.
+from .models import UpcomingEvents
+
+
+class UpcomingEventsView(TemplateView):
+    template_name = "events/upcoming_events.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['upcoming_events'] = UpcomingEvents.objects.all()
+        return context
