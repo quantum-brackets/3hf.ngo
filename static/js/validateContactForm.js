4f805e4f6adac252a799d5f5ccecdf08d8506b29
hengage <tunecarterkillz@gmail.com>
2024-05-26 06:18:38 +0100

    Feature: validate contct us form

--- static/js/validateContactForm.js
+++ static/js/validateContactForm.js
@@ -1,3 +1,40 @@
-function validateForm (){
-    contactUsForm = document.getElementById('contactUsForm');
+const contactUsForm = document.getElementById("contact-us-form");
+
+const fullName = document.getElementById("id_name");
+const email = document.getElementById("id_email");
+const phoneNumber = document.getElementById("id_phone");
+const message = document.getElementById("id_message");
+
+function validateForErrors(error) {
+  if (!fullName.value.trim() | (fullName.value.trim() === "")) {
+    error.push(fullName.id);
+  }
+
+  if (!email.value.trim() || email.value.trim() === "") {
+    error.push(email.id);
+  }
+
+  if (!message.value.trim() || message.value.trim() === " ") {
+    error.push(message.id);
+  }
+}
+
+function validateContactForm() {
+  const errors = [];
+
+  validateForErrors(errors);
+
+  if (errors.length > 0) {
+    const formError = document.getElementById("contact-form-error");
+    formError.classList.remove("hidden");
+
+    errors.map((errorId) => {
+      const errorElement = document.getElementById(errorId);
+      errorElement.style.border = "3px solid red";
+    });
+
+    return false;
+  }
+
+  return true;
 }
