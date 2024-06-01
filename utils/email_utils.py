ec7a86c34cf1e2f1674bbf890def353dbd8c42f0
hengage <tunecarterkillz@gmail.com>
2024-06-01 01:46:01 +0100

    Fixed: email issues  - wrong email and forwarders

--- utils/email_utils.py
+++ utils/email_utils.py
@@ -1,11 +1,21 @@
 from django.core.mail import send_mail
+from django.core.mail import EmailMessage
+from django.conf import settings
 
 def send_contact_message(name, email, phone_number, message):
-    send_mail(
-        subject='Contact message from 3hf',
-        message=f"Name: {name}\nEmail: {email}\nPhone Number: {phone_number}\n\nMessage: {message}",
-        from_email=email,
-        recipient_list=['program@3hf.ngo'],
+    subject = 'Contact message from 3hf'
+    message_body = f"Name: {name}\nEmail: {email}\nPhone Number: {phone_number}\n\nMessage: {message}"
+    from_email = settings.EMAIL_HOST_USER
+    recipient_list = [settings.EMAIL_HOST_USER]
+    reply_to = [email,]
+
+    email_message = EmailMessage(
+        subject=subject,
+        body=message_body,
+        from_email=from_email,
+        to=recipient_list,
+        reply_to=reply_to,
     )
+    email_message.send()
 
     print("Sent message")
