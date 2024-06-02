3d3b06109f32b5df7f709a42847e6b6714d20f08
hengage <tunecarterkillz@gmail.com>
2024-06-02 18:29:33 +0100

    Feature: event registration model

--- apps/events/admin.py
+++ apps/events/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
 
-from .models import UpcomingEvents, ConcludedEvents
+from .models import UpcomingEvents, ConcludedEvents, EventRegistration
 from .forms import CreateUpcomingEventForm, ConcludedEventsForm
 
 class ConcludedEventsInline(admin.StackedInline):
@@ -9,9 +9,12 @@ class ConcludedEventsInline(admin.StackedInline):
     extra= 0
     form = ConcludedEventsForm
 
+class EventRegistrationInline(admin.StackedInline):
+    model = EventRegistration
+    extra= 0
 @admin.register(UpcomingEvents)
 class UpcomingEventsAdmin(admin.ModelAdmin):
     model = UpcomingEvents
     form = CreateUpcomingEventForm
     list_display = ['theme', 'date', 'location']
-    inlines = [ConcludedEventsInline]
+    inlines = [EventRegistrationInline, ConcludedEventsInline]
--- apps/events/models.py
+++ apps/events/models.py
@@ -4,6 +4,7 @@ from django.template.defaultfilters import slugify
 from cloudinary.models import CloudinaryField
 from django_summernote.models import AbstractAttachment
 
+
 class UpcomingEvents(models.Model):
     theme = models.CharField(max_length=200)
     description = models.TextField()
@@ -13,16 +14,17 @@ class UpcomingEvents(models.Model):
     image = models.ImageField(upload_to='static/uploads')
     createdAt = models.DateTimeField(auto_now_add=True)
     updatedAt = models.DateTimeField(auto_now=True)
-    slug = models.SlugField(max_length=70, null=True, blank=True, editable=False)
+    slug = models.SlugField(max_length=70, null=True,
+                            blank=True, editable=False)
 
     class Meta:
         verbose_name = "event"
         verbose_name_plural = "events"
-        
+
     def __str__(self):
         return f"{self.theme.title()}"
- 
-    def save(self, *args, **kwargs): 
+
+    def save(self, *args, **kwargs):
         slug = f'{self.theme}-{self.date}'
         if not self.slug:
             self.slug = slugify(slug)
@@ -43,11 +45,30 @@ class ConcludedEvents(models.Model):
     def __str__(self):
         return f"{self.event.theme.title()}"
 
-    def save(self, *args, **kwargs): 
+    def save(self, *args, **kwargs):
         slug = self.event.slug
         if not self.slug:
             self.slug = slugify(slug)
         return super().save(*args, **kwargs)
+
+
+class EventRegistration(models.Model):
+    event = models.ForeignKey(
+        UpcomingEvents, related_name='events', on_delete=models.SET_NULL, null=True)
+    registrant_name = models.CharField(blank=False, max_length=250)
+    registrant_email = models.EmailField(blank=False, max_length=250)
+    registrant_phone_number = models.CharField(blank=False, max_length=25)
+    additional_message = models.TextField(
+        blank=True, null=True, max_length=250)
+    registered_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.registrant_name} - {self.registrant_email}"
+    
+    class Meta:
+        verbose_name = "Registered person"
+        verbose_name_plural = "registered persons"
 
 
 class SummernoteAttachment(AbstractAttachment):
