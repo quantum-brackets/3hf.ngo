cb4031933adb06c5435414a8cdc3d4c3c13122bf
hengage <tunecarterkillz@gmail.com>
2024-06-06 02:14:07 +0100

    Refactor: remove 'concluded' from concluded  events url path

--- apps/events/urls.py
+++ apps/events/urls.py
@@ -6,7 +6,7 @@ from . import views
 urlpatterns = [
    path('upcoming/', views.UpcomingEventsView.as_view(), name='upcoming_events'),
    path('upcoming/<slug:slug>/', views.event_detail_json, name='event-detail-json'),
-   path('concluded/', views.ConcludedEventsListView.as_view(), name='concluded_events'),
-   path('concluded/<slug:slug>/', views.ConcludedEventsDetailsView.as_view(), name='concluded_event_details'),
+   path('', views.ConcludedEventsListView.as_view(), name='concluded_events'),
+   path('<slug:slug>/', views.ConcludedEventsDetailsView.as_view(), name='concluded_event_details'),
    path('upcoming/<int:event_id>/register/', views.register_for_event, name='event_registration'),
 ]
