d9945298668d63793f2a81383ae5036a9ec99d95
hengage <tunecarterkillz@gmail.com>
2024-06-07 01:49:50 +0100

    Feature: open event location in map

---
+++ static/js/locationMapHandler.js
@@ -0,0 +1,13 @@
+document.getElementById('map-div').addEventListener('click', function() {
+    var address = document.getElementById('event-location').innerText;
+    var googleMapsAppUrl = 'comgooglemaps://?q=' + encodeURIComponent(address);
+    var googleMapsWebUrl = 'https://www.google.com/maps/search/?api=1&query=' + encodeURIComponent(address);
+
+    // Try to open the Google Maps app
+    window.location = googleMapsAppUrl;
+
+    // If the Maps app is not available, fallback to opening in a web browser after a short delay
+    setTimeout(function() {
+        window.open(googleMapsWebUrl, '_blank')
+    }, 500);
+});
