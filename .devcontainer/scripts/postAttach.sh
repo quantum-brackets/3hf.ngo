e6693c101825e91cc15f6cbbeb07fd27650693c6
Emmanuel Ogbizi <iamogbz+github@gmail.com>
2024-06-13 02:55:54 +0000

    chore: make dev port 8000 public for testing

--- .devcontainer/scripts/postAttach.sh
+++ .devcontainer/scripts/postAttach.sh
@@ -5,4 +5,6 @@ npm install
 python -m venv venv
 . venv/bin/activate
 pip install -r requirements.txt
+python -m manage migrate
+gh codespace ports visibility 8000:public -c $CODESPACE_NAME
 python -m manage runserver
